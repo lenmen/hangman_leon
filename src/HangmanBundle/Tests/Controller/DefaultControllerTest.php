d3f33c7438db55e52a294a9ac6a917036d340130
Lennard Moll <l.moll@simgroup.nl>
2016-01-15 14:55:59 +0100

    Working atm. with debugging

---
+++ src/HangmanBundle/Tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lennard
+ * Date: 15-1-16
+ * Time: 14:46
+ */
+
+namespace HangmanBundle\Tests\Controller;
+
+
+class DefaultControllerTest extends WebTes
+{
+
+}
